b8948714c783fe6a952a30b60b95bbbfe8a9c071
Maksim Grigorev <grigorevmg@mai.ru>
2025-04-07 20:55:59 +0300

    test

--- LAB_26/LAB/main.c
+++ LAB_26/LAB/main.c
@@ -3,17 +3,17 @@
 #include "deque_str.h"
 
 int main() {
-    deque_str* d = dstr_create(10);
-    dstr_push_back(d, "Hello");
-    dstr_push_front(d, "World");
+    // deque_str* d = dstr_create(10);
+    // dstr_push_back(d, "Hello");
+    // dstr_push_front(d, "World");
     
-    char* s1 = dstr_pop_front(d); // "World"
-    printf("%s\n", s1);
-    free(s1);
+    // char* s1 = dstr_pop_front(d); // "World"
+    // printf("%s\n", s1);
+    // free(s1);
     
-    char* s2 = dstr_pop_back(d); // "Hello"
-    printf("%s\n", s2);
-    free(s2);
+    // char* s2 = dstr_pop_back(d); // "Hello"
+    // printf("%s\n", s2);
+    // free(s2);
     
-    dstr_destroy(d);
+    // dstr_destroy(d);
 }
