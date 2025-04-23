088d55dd7bc31c292e9a4460b4eb6f00b491ff1f
Girday <123688667+Girday@users.noreply.github.com>
2025-04-24 00:06:27 +0300

    jenya force

--- LAB_24/Lab24_Grigorev_30/stack_lex/stack_lex.h
+++ LAB_24/Lab24_Grigorev_30/stack_lex/stack_lex.h
@@ -1,7 +1,8 @@
 #pragma once
+#include "../token/token.h"
 
 typedef struct {
-    char* buf;
+    Token* buf; // Изменено с char* на Token*
     int max_len;
     int len;
 } stack_lex;
@@ -9,6 +10,6 @@ typedef struct {
 stack_lex* slex_create(int max_len);
 void slex_destroy(stack_lex* obj);
 int slex_is_empty(stack_lex* obj);
-int slex_push_back(stack_lex* obj, char value);
-char slex_pop_back(stack_lex* obj);
-char slex_top(stack_lex* obj);
+int slex_push_back(stack_lex* obj, Token value); // Изменено char на Token
+Token slex_pop_back(stack_lex* obj); // Изменено char на Token
+Token slex_top(stack_lex* obj); // Изменено char на Token
